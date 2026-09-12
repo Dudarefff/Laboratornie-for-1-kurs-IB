eab884fc0e7be022a704aa990d54251f9b3db451
иван дударев <dudarevivan454@gmail.com>
2026-09-12 18:22:16 +0300

    сделано задание 4

---
+++ lab1/swap.py
@@ -0,0 +1,8 @@
+first_room=input('номер первой аудитории: ')
+second_room=input('номер второй аудитории: ')
+swap=first_room
+first_room=second_room
+second_room=swap
+print('ЗАМЕНА!!!')
+print('первая аудитория: ', first_room)
+print('вторая аудитория: ', second_room)
